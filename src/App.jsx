f8ef75d199257a0cf8812a37f4a5dbdbaff72878
Tanzzzzzzzzz <Tanay.75.1205@gmail.com>
2025-05-13 15:21:44 +0530

    Tailwind reconfigured

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-
-import React, { useState } from 'react';
-import Node from './components/Node';
-import './styles/canvas.css';
-
 function App() {
-  const [nodes, setNodes] = useState([]);
-  
-  const handleAddNode = (e) => {
-    const newNode = {
-      id: Date.now(),
-      x: e.clientX,
-      y: e.clientY
-    };
-    setNodes([...nodes, newNode]);
-  };
-
   return (
-    <div className="canvas" onClick={handleAddNode}>
-      {nodes.map((node) => (
-        <Node key={node.id} x={node.x} y={node.y} />
-      ))}
+    <div className="text-3xl font-bold text-center text-blue-500">
+      Tailwind is working!
     </div>
   );
 }
